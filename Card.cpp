1ce1c97826b4df8bb570ba2c5c3d23674abd6168
txh <2771988734@qq.com>
2021-06-03 10:41:57 +0800

    perfected the card class

--- Card.cpp
+++ Card.cpp
@@ -1,6 +1,12 @@
 #include"Card.h"
 int luck;
 
+Card::Card(std::string _event, int _val)
+{
+	event = _event;
+	val = _val;
+}
+
 double Card::LuckVal()  //计算幸运值，幸运值与抽到的卡片有关
 {
 	luck = 1 - luck * 0.9;  //计算luck值
--- Chance.h
+++ Chance.h
@@ -1,10 +1,13 @@
-#pragma once
+#ifndef CHANCE_H
+#define CHANCE_H
+
 #include"Card.h"
 
-//机会类  好的多
+//机会类 （好的多）
 class Chance :public Card
 {
 public:
-	Chance();  //构造函数，初始化事件名称
-	void Func();  //事件具体操作
+	Chance(std::string _name, int _val) { Card(_name, _val); };  //构造函数，初始化事件名称
 };
+
+#endif
