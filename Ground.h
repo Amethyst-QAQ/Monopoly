00b4463af51e9f6c9665a78363146bda9820d669
txh <2771988734@qq.com>
2021-06-03 14:33:38 +0800

    change the type of onStepped and onPassed

--- Ground.h
+++ Ground.h
@@ -4,8 +4,8 @@
 class Ground
 {
 public:
-	virtual void onStepped() = 0;	//路过的时候调用，纯虚函数
-	virtual void passed() = 0;		//经过的时候调用，纯虚函数
+	virtual void onStepped() {};	//路过的时候调用，纯虚函数
+	virtual void onPassed() {};		//经过的时候调用，纯虚函数
 };
 
 #endif
