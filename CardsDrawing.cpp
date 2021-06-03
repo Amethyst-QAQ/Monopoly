fb7d0863f0fc0ceddcf672489756d11995333316
Sweetie-he <2771988734@qq.com>
2021-06-03 16:14:20 +0800

    amended the rule of drawing cards.

--- CardsDrawing.cpp
+++ CardsDrawing.cpp
@@ -1,6 +1,5 @@
-#include"Person.h"
-#include<iostream>
-#include<ctime>
+#include "CardsDrawing.h"
+
 using namespace std;
 
 //抽卡系统
@@ -13,18 +12,15 @@ void LuckVal(Person &a)          //计算幸运值，幸运值与抽到的卡片有关
 	a.setLuck(luck);              //更新Person的Luck值
 }
 
-float CardsDrawing(Person &a)
+Card* CardsDrawing(Person &a)
 {
 	srand((unsigned int)time(NULL));                //根据系统时间生成随机数
-	if (a.getLuck() >= 0.65 && a.getLuck() <= 1)    //好卡
+	if (a.getLuck() >= 0.6 && a.getLuck() <= 1)
 	{
-		//Chance中：好卡7张，坏卡3张
-		//Fate中：好卡3张，坏卡7张
-		cout << "The number of the card you draw is " << 1 + rand() % 10 << endl;
-		LuckVal(a);                                 //抽卡后更改幸运值
-	}
-	if (a.getLuck() >= 0 && a.getLuck() < 0.65)
-	{
-		cout << "The number of the card you draw is " << 1 + rand() %  << endl;
+		int n = rand() % 8;								//输出一个0-8的随机数
+		if (n == 4 || n == 5)	n = 3;
+		if (n == 6 || n == 7) n = 4;
+		if (n == 8) n = 5;
+		int m = rand() % 3;//输出一个0-3的随机数
 	}
 }
---
+++ CardsDrawing.h
@@ -0,0 +1,19 @@
+#ifndef CARD_DRAWING_H
+#define CARD_DRAWING_H
+
+#include"Person.h"
+#include "Card.h"
+#include<iostream>
+#include<ctime>
+
+class CardsDrawing
+{
+private:
+
+};
+
+void LuckVal(Person& a);
+
+Card* CardsDrawing(Person& a);
+
+#endif
