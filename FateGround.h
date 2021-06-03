0c73bbdf4ab40dd1aa94bd80613f9ded472001b4
BigBigShrimp <1131470174@qq.com>
2021-06-03 22:24:37 +0800

    alter CardsDrawing.cpp

--- FateGround.h
+++ FateGround.h
@@ -1,12 +1,13 @@
 #ifndef FATEGROUND_H
 #define FATEGROUND_H
+#include"Ground.h"
 
 //命运格子类
-class FateGround
+class FateGround:public Ground
 {
 public:
-	bool onFground() { return 1; }         //经过幸运格子
-	bool Not_onFground() { return 0; }     //路过幸运格子
+	bool onFground() { return 1; }         //经过命运格子
+	bool Not_onFground() { return 0; }     //路过命运格子
 };
 #endif // !FATEGROUND_H
 
