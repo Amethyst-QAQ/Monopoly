306226e75264b15d05f5dcc2d3049d657e6f28f0
Sweetie-he <2771988734@qq.com>
2021-06-04 00:43:11 +0800

    rewrite other classes which inherited card class

--- CardsDrawing/Chance.h
+++ CardsDrawing/Chance.h
@@ -7,7 +7,7 @@
 class Chance : public Card
 {
 public:
-	Chance(std::string _name, int _val, int _num):Card(_name, _val, _num){}  	//构造函数，初始化事件名称，设置这张卡的幸运值
+	Chance(std::string _name, int _val): Card(_name, _val) {}  	//构造函数，初始化事件名称，设置这张卡的幸运值
 };
 
 #endif
