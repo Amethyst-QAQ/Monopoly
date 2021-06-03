17ad808ac5265e543ac1e524407012945c2afe68
txh <2771988734@qq.com>
2021-06-03 09:43:19 +0800

    Perfected card class

--- Card.h
+++ Card.h
@@ -1,12 +1,16 @@
-#pragma once
-#include<iostream>  
+#ifndef CARD_H
+#define CARD_H
+
+#include<iostream>
 
 //卡片类
 class Card
 {
-protected:
+private:
 	std::string m_Eventname[10];  //存放事件名称
 public:
-	virtual void doEvent();  //事件操作
-	double LuckVal();  //计算幸运值，幸运值与抽到的卡片有关
+	virtual void doEvent() = 0;  //事件操作
+	float LuckVal();  //计算幸运值，幸运值与抽到的卡片有关
 };
+
+#endif
