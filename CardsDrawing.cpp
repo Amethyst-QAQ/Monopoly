b35bb3ac344e083fa5b4db7848b49d3a767b87bf
BigBigShrimp <1131470174@qq.com>
2021-06-03 14:48:16 +0800

    complete CardsDrawing.cpp

--- CardsDrawing.cpp
+++ CardsDrawing.cpp
@@ -1,4 +1,9 @@
 #include"Person.h"
+#include<iostream>
+#include<ctime>
+using namespace std;
+
+//抽卡系统
 
 void LuckVal(Person &a)          //计算幸运值，幸运值与抽到的卡片有关
 {
@@ -6,4 +11,19 @@ void LuckVal(Person &a)          //计算幸运值，幸运值与抽到的卡片有关
 	luck = 1 - luck * 0.9;        //计算luck值
 	                              //好卡坏卡交替出现
 	a.setLuck(luck);              //更新Person的Luck值
+}
+
+float CardsDrawing(Person &a)
+{
+	srand((unsigned int)time(NULL));                //根据系统时间生成随机数
+	if (a.getLuck() >= 0.65 && a.getLuck() <= 1)    //好卡
+	{
+		//Chance中：好卡7张，
+		cout << "The number of the card you draw is " << 1 + rand() % 10 << endl;
+		LuckVal(a);                                 //抽卡后更改幸运值
+	}
+	if (a.getLuck() >= 0 && a.getLuck() < 0.65)
+	{
+		cout << "The number of the card you draw is " << 1 + rand() %  << endl;
+	}
 }
