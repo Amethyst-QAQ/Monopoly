c648b290620efbe619a89827fd53c590d5aedbdb
Surveyous <2118342460@qq.com>
2021-06-04 19:45:08 +0800

    Completed the Transportation and Beginning classes

--- Ground/Transportation.h
+++ Ground/Transportation.h
@@ -8,19 +8,17 @@
 //收费站格子
 class Transportation :public Ground
 {
-private:
-	std::string name;                //收费站名称
+protected:
 	int toll;                        //基础过路费
 	int dice;                        //骰子点数
 public:
-	Transportation(std::string _name, int _toll) :name(_name), toll(_toll), dice(0) {}
+	Transportation() : toll(0), dice(0) {}
 
 	void onStepped(Person& a)        //路过时调用
 	{
 		a.setMoney(a.getMoney() - toll * dice);
 	}
 	void setDice(int _dice) { dice = _dice; }
-	std::string	getName() { return name; }
 	int getToll() { return toll; }
 };
 
