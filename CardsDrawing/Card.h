5b3736e78c25074cea22c73cc32908fe079d99f7
BigBigShrimp <1131470174@qq.com>
2021-06-05 10:58:50 +0800

    alter ChanceGround.h and CardsDrawing.h, build CardsSuit.h and ChanceSuit.h,

--- CardsDrawing/Card.h
+++ CardsDrawing/Card.h
@@ -5,18 +5,19 @@
 #include<string>
 
 ///卡片类
+
 class Card
 {
 private:
 	std::string m_name;				                           //存放事件名称
 	int m_val;					  	                           //卡的好坏,分为0~5共6个等级,数字越小越坏
-
+	int m_num;                                                 //卡在每个等级中的编号
 public:
-	Card(std::string name,int val):m_name(name),m_val(val){}   //构造函数，初始化卡的事件名称与幸运值
-	
+	Card(std::string name,int val,int num):m_name(name),m_val(val),m_num(num){}   
+	                                                           //构造函数，初始化卡的事件名称与幸运值
 	std::string getName() { return m_name; }                   //获得卡的名称
 	int getVal() { return m_val; }                             //获得卡的幸运值
-	
+	int getNum() { return m_num; }                             //获得卡的编号
 	virtual void doEvent() {};                                 //卡的执行操作，设为虚函数，需要子类重写
 }; 
 
--- CardsDrawing/CardsDrawing.h
+++ CardsDrawing/CardsDrawing.h
@@ -10,9 +10,16 @@
 
 class CardsDrawing
 {
+private:
+	int m_val;   //抽到的牌的等级
+	int m_num;   //抽到的牌的编号
 public:
 	void LuckVal(Person& a);
 	Card* Draw(Person& a);
+	void setVal(int num) { m_val = num; }  //输入抽到的牌的等级
+	void setNum(int num) { m_num = num; }  //输入抽到的牌的编号
+	int getVal() { return m_val; }         //获取抽到的牌的等级
+	int getNum() { return m_num; }         //获取抽到的牌的编号
 };
 
 #endif
