dbcbdc1d346982b3a1882a88d8a45ed2e940bc3a
BigBigShrimp <1131470174@qq.com>
2021-06-05 23:19:23 +0800

    completed the CardsSuit , ChanceSuit and FateSuit class

--- CardsDrawing/FateSuit.h
+++ CardsDrawing/FateSuit.h
@@ -4,16 +4,4 @@
 #include<iostream>
 #include "../Player/Person.h/"
 
-class Fate_1 :public Fate                   //发电厂+1
-{
-public:
-	Fate_1() :Fate("为爱发电", 5,0) {}         //构造函数，初始化Fate_1卡的事件名称、幸运值、编号
-
-	void doEvent(Person& a)
-	{
-		std::cout << getName() << endl;
-		a.buyPowerStation();
-	}
-};
-
 #endif // !FATE_H
