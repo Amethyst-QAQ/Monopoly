4bd1469121a6328a58bf28a237b4a15f15386ce5
Chlamydomonos <2457242458@qq.com>
2021-06-06 12:06:52 +0800

    Added Map and fixed some problems

--- Ground/Map.h
+++ Ground/Map.h
@@ -1,8 +1,27 @@
 #ifndef GROUND_MAP_H
 #define GROUND_MAP_H
 
-#include <list>
+#include "Ground.h"
 
+#include <vector>
 
+struct MapNode
+{
+	Ground *ground;
+	MapNode *next;
+	MapNode *prev;
+	MapNode(Ground *_ground, MapNode *_prev) : ground(_ground), next(nullptr), prev(_prev) {}
+	~MapNode() { delete ground; }
+};
+
+class Map
+{
+private:
+	MapNode *beginning;
+public:
+	Map(std::vector<Ground*> allGrounds);
+	~Map();
+	Ground *findGroundFromPosition(Ground *position, int distance);
+};
 
 #endif
