b101b6cb25d303c5b84b89d9b9a3a93196620252
Sweetie-he <2771988734@qq.com>
2021-06-06 18:57:36 +0800

    Completed the main function and debugs

--- Ground/Map.h
+++ Ground/Map.h
@@ -5,12 +5,19 @@
 
 #include <vector>
 
+class Player;
+extern Player* players;
+
 struct MapNode
 {
 	Ground *ground;
 	MapNode *next;
 	MapNode *prev;
-	MapNode(Ground *_ground, MapNode *_prev) : ground(_ground), next(nullptr), prev(_prev) {}
+	MapNode(Ground *_ground, MapNode *_prev) : ground(_ground), next(nullptr), prev(_prev)
+	{
+		if(_prev != nullptr)
+			_prev->next = this;
+	}
 	~MapNode() { delete ground; }
 };
 
@@ -18,10 +25,13 @@ class Map
 {
 private:
 	MapNode *beginning;
+	MapNode* findNodeFromPosition(Ground* position, int distance);
 public:
 	Map(std::vector<Ground*> allGrounds);
 	~Map();
 	Ground *findGroundFromPosition(Ground *position, int distance);
+	void movePlayer(int player, int dice);
+	void initPlayers();
 };
 
 #endif
