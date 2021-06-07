b8cc33ae43122b6b01a7cb08383aa83acb31cc3f
Sweetie-he <2771988734@qq.com>
2021-06-07 10:54:25 +0800

    Designing of Stock System

--- Financial/Stock.cpp
+++ Financial/Stock.cpp
@@ -2,23 +2,25 @@
 #include<iostream>
 using namespace std;
 
-Stock::Stock()
+Stock::Stock(float _initialPrice, int _num) : initialPrice(_initialPrice), 
+											  actualPrice(_initialPrice), 
+											  num(_num)
 {
-	initialPrice = 8;
-	actualPrice = 0;
+	soldPrice = new float[_num];
+	soldNum = new int[_num];
+	for (int i = 0;i < num;i++)
+	{
+		soldPrice[i] = 0;
+		soldNum[i] = 0;
+	}
 }
 
 void Stock::setActualPrice()
 {
 	int n;
-	srand((unsigned int)time(NULL));
+	//srand((unsigned int)time(NULL));		//写在main里
 	n = rand() % (21) - 10;                 //生成 -10 到 10 的随机数
 	actualPrice = initialPrice * (1 + n / 100);
-}
-
-void Stock::resetActualPrice()
-{
-	initialPrice = actualPrice;
 }
 
 void Stock::buyStock(int player)
@@ -26,9 +28,9 @@ void Stock::buyStock(int player)
 	Player& p = players[player];
 
 	int money;
-	int num;
+	int sum;
 
-	string mainText = "Please choose the buying price you want.";
+	string mainText = "Please choose the price you want.";
 	vector<string> operation = { "In actual price(bank's)", "In player's price" };
 	string serverText = "buy_stock";
 
@@ -44,7 +46,7 @@ void Stock::buyStock(int player)
 		Output::instance->print(mainText, serverText);
 
 		n = menu.exec();
-		while (n < 100 || n>1200)
+		while (n < 100 || n > 1200)
 		{
 			mainText = "Wrong number! Please enter again! ";
 			serverText = "default_buy_from_bank";
@@ -59,8 +61,8 @@ void Stock::buyStock(int player)
 
 
 		//买入股票操作 个人持有股数增加 钱减少
-		num = p.getStockNum();
-		num += n;
+		sum = p.getStockNum();
+		sum += n;
 		p.setStockNum(n);         //成功买入，个人持股数增加
 
 		money = p.getMoney();
@@ -80,11 +82,11 @@ void Stock::sellStock(int player)
 	Player& p = players[player];
 
 	int money;
-	int num;
+	int sum;
 
 	string mainText = "Please choose the selling price you want.";                                   // 实时定价 or 自己定价
 	vector<string> operation = { "In actual price", "In your price" };
-	string serverText = "sell_stock_price";
+	string serverText = "sell_price";
 
 	Menu menu(mainText, operation, serverText);
 
@@ -92,43 +94,51 @@ void Stock::sellStock(int player)
 
 	if (n == 1) //In actual price
 	{
-		mainText = "Please enter the amount you want.\n(Attention!!! youHave ≤ Amount ≤ 800)";
-		serverText = "sell_youramount";
+		mainText = "Please enter the amount you want to sell.\n(Attention!!! youHave ≤ Amount ≤ 800)";
+		serverText = "sell_amount";
 
 		Output::instance->print(mainText, serverText);
 
-		while (n < p.getStockNum() || n>800)
+		scanf("%d", &n);
+
+		sum = p.getStockNum();//获取现有股数
+
+		while (n < sum || n > 800)
 		{
 			mainText = "Wrong amount!\nPlease enter again!";
-			serverText = "Wrong_sell_myprice_bank";
+			serverText = "Wrong_amount";
 
 			Output::instance->print(mainText, serverText);
-			n = menu.exec();
+			scanf("%d", &n);
 		}
-		mainText = "Successfully sell " + to_string(n) + " shares!";
-		serverText = "Success_sell_actualprice_bank";
+
+		mainText = "Successfully sold " + to_string(n) + " shares!";
+		serverText = "Successful_sold";
 
 		Output::instance->print(mainText, serverText);
 
-		p.setStockNum(n);         //设置自己挂出的股数
-
-		//卖出股票操作 个人持有股数减少 钱增加
-		num = p.getStockNum();
-		num -= n;
-		p.setStockNum(num);
-
+		sum -= n;
+		p.setStockNum(sum);		  //个人持股数减少
 		money = p.getMoney();
 		money += n * actualPrice;
-		p.setMoney(money);
+		p.setMoney(money);		  //个人钱数增加
+
+		soldNum[player] = n;
+		soldPrice[player] = actualPrice;
 	}
 	else       //In your price
 	{
-		mainText = "Please enter the price you want.";
-		serverText = "set_sell_yourprice";
+		mainText = "Please enter the price you want.\n";//有问题
+		serverText = "set_sell_price";
 
 		Output::instance->print(mainText, serverText);
-		n = menu.exec();
-		p.setmyStockPrice(n);    //设置自己的出售定价
+		scanf("%d", &n);
+
+		while (n > 1.1 * actualPrice || n < 0.9 * actualPrice)
+		{
+			mainText = "Wrong Price!\nPlease enter again!";
+		}
+
 
 		mainText = "Please enter the amount you want.\n(Attention!!! youHave ≤ Amount ≤ 800)";
 		serverText = "sell_youramount";
@@ -151,12 +161,11 @@ void Stock::sellStock(int player)
 		p.setStockNum(n);         //设置自己挂出的股数
 
 		//卖出股票操作 个人持有股数减少 钱增加
-		num = p.getStockNum();
-		num -= n;
-		p.setStockNum(num);
+		sum = p.getStockNum();
+		sum -= n;
+		p.setStockNum(sum);
 
 		money = p.getMoney();
-		money += n * p.getmyStockPrice();
 		p.setMoney(money);
 	}
 
--- Financial/Stock.h
+++ Financial/Stock.h
@@ -8,21 +8,31 @@
 #include "../Player/Player.h"
 extern Player* players;
 
-using namespace std;
-
-
 //股票类
 class Stock
 {
 private:
 	float initialPrice;   //开盘价
 	float actualPrice;    //实时价格
+	float* soldPrice;	  //玩家出售价格
+	int* soldNum;		  //玩家出售数量
+	int num;			  //玩家数量
 public:
-	Stock();
+	Stock() : initialPrice(8), actualPrice(8), num(4)
+	{
+		soldPrice = new float[4]{ 0,0,0,0 };
+		soldNum = new int[4]{ 0,0,0,0 };
+	}
+	Stock(float _initialPrice) : initialPrice(_initialPrice), actualPrice(_initialPrice), num(4)
+	{
+		soldPrice = new float[4]{ 0,0,0,0 };
+		soldNum = new int[4]{ 0,0,0,0 };
+	}
+	Stock(float _initialPrice, int _num);
 
 	void setActualPrice();
 
-	void resetActualPrice();
+	void resetActualPrice() { initialPrice = actualPrice; }
 
 	void buyStock(int player);
 	
