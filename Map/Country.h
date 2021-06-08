260779e2823b5a848ad58ae5292628a34febe761
Chlamydomonos <2457242458@qq.com>
2021-06-08 19:09:21 +0800

    Completed country

---
+++ Map/Country.h
@@ -0,0 +1,28 @@
+#ifndef MAP_COUNTRY_H
+#define MAP_COUNTRY_H
+
+#include "GroundWithPrice.h"
+
+class Country : public GroundWithPrice
+{
+private:
+	int color;
+	int houseNum;
+	int getBaseToll();
+public:
+	Country(const std::string &name, int price, int _color):
+		GroundWithPrice(name, price),
+		color(_color),
+		houseNum(0){}
+
+	virtual int getToll();
+	int getColor() { return color; }
+	int getHousePrice();
+
+	void build(Player *player);
+
+	virtual void buy(Player *player);
+	virtual void onStepped(Player *player);
+};
+
+#endif // !MAP_COUNTRY_H
