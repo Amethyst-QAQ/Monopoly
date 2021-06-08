676abf619375be0dc7656c90e17ec35307c4ae34
Sweetie-he <2771988734@qq.com>
2021-06-08 22:44:06 +0800

    Init the FateGround

--- CardsDrawing/FateSuit/Fate_1.h
+++ CardsDrawing/FateSuit/Fate_1.h
@@ -7,12 +7,11 @@
 class Fate_1 :public Fate                   //发电厂+1
 {
 public:
-	Fate_1() :Fate("为爱发电", 5, 0) {}         //构造函数，初始化Fate_1卡的事件名称、幸运值、编号
+	Fate_1() :Fate("为爱发电", 5) {}         //构造函数，初始化Fate_1卡的事件名称、幸运值、编号
 
 	void doEvent(Player& a)
 	{
 		Fate::doEvent();
-		a.buyPowerStation();
 	}
 };
 
