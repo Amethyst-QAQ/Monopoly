afc552d8f1d5bb47dbe6b1ac6e81ffa9353e1663
Chlamydomonos <2457242458@qq.com>
2021-06-09 00:53:42 +0800

    Fixed things

--- Core/main.cpp
+++ Core/main.cpp
@@ -1,10 +1,14 @@
 #include "GameLogic.h"
 #include <string>
+#include <cstdlib>
+#include <time.h>
 
 bool isServer = false;
 
 int main(int argc, char **argv)
 {
+	srand(time(NULL));
+	std::srand(time(NULL));
 	if (argc == 2 && argv[1] == std::string("server"))
 		isServer = true;
 	GameLogic logic;
